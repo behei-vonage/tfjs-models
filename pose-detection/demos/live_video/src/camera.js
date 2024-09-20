a4b53842c368a8c247ca67742560d092370e1e6d
Misha Behei <misha.behei@vonage.com>
2024-09-20 15:21:49 -0500

    some stuff

--- pose-detection/demos/live_video/src/camera.js
+++ pose-detection/demos/live_video/src/camera.js
@@ -16,6 +16,7 @@
  */
 import * as params from './params';
 import {isMobile} from './util';
+import OT from '@opentok/client';
 
 export class Camera {
   constructor() {
@@ -27,50 +28,85 @@ export class Camera {
    * @param cameraParam From app `STATE.camera`.
    */
   static async setup(cameraParam) {
-    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-      throw new Error(
-          'Browser API navigator.mediaDevices.getUserMedia not available');
-    }
+    let session;
+    let apiKey;
+    let sessionId;
+    let token;
+    let streamId; 
+    session = OT.initSession(apiKey, sessionId);
 
-    const {targetFPS, sizeOption} = cameraParam;
-    const $size = params.VIDEO_SIZE[sizeOption];
-    const videoConfig = {
-      'audio': false,
-      'video': {
-        facingMode: 'user',
-        // Only setting the video to a specified size for large screen, on
-        // mobile devices accept the default size.
-        width: isMobile() ? params.VIDEO_SIZE['360 X 270'].width : $size.width,
-        height: isMobile() ? params.VIDEO_SIZE['360 X 270'].height :
-                             $size.height,
-        frameRate: {
-          ideal: targetFPS,
-        }
-      }
+    // Subscribe to a newly created stream
+    session.on('streamCreated', function streamCreated(event) {
+    var subscriberOptions = {
+      insertDefaultUI: false,
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
     };
-
-    const stream = await navigator.mediaDevices.getUserMedia(videoConfig);
-
-    const camera = new Camera();
-    camera.video.srcObject = stream;
-
-    await new Promise((resolve) => {
-      camera.video.onloadedmetadata = () => {
-        resolve(video);
-      };
+    var subscriber = session.subscribe(event.stream, 'subscriber', subscriberOptions);
+    subscribers[subscriber.streamId] = subscriber;
     });
 
-    camera.video.play();
+    session.on('sessionDisconnected', function sessionDisconnected(event) {
+      console.log('You were disconnected from the session.', event.reason);
+    });
 
-    const videoWidth = camera.video.videoWidth;
-    const videoHeight = camera.video.videoHeight;
-    // Must set below two lines, otherwise video element doesn't show.
-    camera.video.width = videoWidth;
-    camera.video.height = videoHeight;
+    // initialize the publisher
+    var publisherOptions = {
+      insertDefaultUI: false,
+      name: 'Publisher',
+      publishAudio: true,
+      publishVideo: true,
+    };
+    var publisher = OT.initPublisher('publisher', publisherOptions);
 
-    const canvasContainer = document.querySelector('.canvas-wrapper');
-    canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
+    // Connect to the session
+    session.connect(token, function callback(error) {
+      if (error) {
+        console.warn(error);
+      } else {
+        // If the connection is successful, publish the publisher to the session
+        session.publish(publisher, async (err) => {
+          if (err) {
+            console.log('Error', err);
+          } else {
+            publisher.on('mediaStreamAvailable', (mediaStream) => {
+              console.warn('mediaStreamAvailable');
+              const {targetFPS, sizeOption} = cameraParam;
+              const $size = params.VIDEO_SIZE[sizeOption];
+              const videoConfig = {
+                'audio': false,
+                'video': {
+                  facingMode: 'user',
+                  // Only setting the video to a specified size for large screen, on
+                  // mobile devices accept the default size.
+                  width: isMobile() ? params.VIDEO_SIZE['360 X 270'].width : $size.width,
+                  height: isMobile() ? params.VIDEO_SIZE['360 X 270'].height :
+                                      $size.height,
+                  frameRate: {
+                    ideal: targetFPS,
+                  }
+                }
+              };
 
-    return camera;
+              const stream = mediaStream;
+
+              const camera = new Camera();
+              camera.video.srcObject = stream;
+              const videoWidth = camera.video.videoWidth;
+              const videoHeight = camera.video.videoHeight;
+              // Must set below two lines, otherwise video element doesn't show.
+              camera.video.width = videoWidth;
+              camera.video.height = videoHeight;
+
+              const canvasContainer = document.querySelector('.canvas-wrapper');
+              canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
+
+              return camera;
+            })
+          }
+        });
+      }
+    });
   }
 }
